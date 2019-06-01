8546c3b0ddc1ce4e8bdbf55e8eaa98c5cef96501
Kirill Gorshkoff <gobzter@gmail.com>
2019-06-01 14:02:58 +0300

    HW5

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,27 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+import collections
+
+med = []
+
+corps = collections.defaultdict(list)
+n = int(input('Введите количество предприятий: '))
+
+for i in range(n):
+    corp_name = input(str(i+1) + "-я компания: ")
+
+    for j in range(4):
+        quarter = int(input(f'Прибыль за {j+1}-й квартал: '))
+        corps[corp_name].append(quarter)
+
+    corps[corp_name].append(sum(corps[corp_name]) / 4)
+    med.append(corps[corp_name][4])
+
+print(f'Средняя прибыль за год для всех предприятий равна {sum(med) / n}')
+
+for k, v in corps.items():
+    if v[-1] < sum(med) / n:
+        print(f'У компании {k} средняя прибыль ниже среднего')
+    if v[-1] > sum(med) / n:
+        print(f'У компании {k} средняя прибыль ниже среднего')
