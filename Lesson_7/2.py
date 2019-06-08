045b5446f46f9529a59b245fb83988e2d35facbf
Kirill Gorshkoff <gobzter@gmail.com>
2019-06-08 12:01:28 +0300

    HW7

--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,44 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+
+
+def merge_sort(lst):
+    if len(lst) > 1:
+        center = len(lst) // 2
+        left = lst[:center]
+        right = lst[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lst[k] = left[i]
+                i += 1
+            else:
+                lst[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            lst[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            lst[k] = right[j]
+            j += 1
+            k += 1
+        return lst
+
+
+lst = [-64, -6, -60, 38, -91, 68, -100, -69, -75, 15, 85, -60, 28, -19, 86, 79, 33, -76, 86, 29, 64, -80, -1, 13, 46, 1, 1, -46, 25, -39, 83, -35, -48, -22, -82, -2, 33, -80, 2, -60, 26, 61, 52, 38, -14, 52, -79, 71, -36, 6]
+print(lst)
+print(merge_sort(lst))
+
+
+
+
--- Lesson_7/3.py
+++ Lesson_7/3.py
@@ -6,3 +6,45 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
  который не рассматривался на уроках
 """
+
+
+def cocktail_sort(lst):
+    n = len(lst)
+    swapped = True
+    start = 0
+    end = n - 1
+
+    while swapped:
+
+        swapped = False
+
+        for i in range(start, end):
+            if lst[i] > lst[i + 1]:
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]
+                swapped = True
+
+        if not swapped:
+            break
+
+        swapped = False
+
+        end = end - 1
+
+        for i in range(end - 1, start - 1, -1):
+            if lst[i] > lst[i + 1]:
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]
+                swapped = True
+
+        start = start + 1
+
+    return lst
+
+
+lst = [63, 9, 70, 11, 38, 12, 76, 63, 95, 35, -18, 8, -3, 21, 81, 56, 35, 50, -31, -88, -99, -60, 91, 81, -98, -45, -22, -8, -56, -34, 30, -61, 73, 65, 35, 24, -50, 81, 23, -93, -19, 7, -22, 46, 36, -52, -87, 2, -89]
+
+
+print(lst)
+cocktail_sort(lst)
+print(lst)
+med = int(len(lst) / 2)
+print(f'Медиана в этом массива находится на позиции {med} и это элемент {lst[med]}')
